b58511526c2783cab01e0d93529ae42e9a33be80
Mike Hashimoto <mike.hashimoto1@gmail.com>
2017-05-22 21:13:49 -0400

    final js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,42 +8,63 @@ $(document).ready(function() {
 
 	var counter = 0;
 
-	var arr = []
+	var arr = [];
+	var winnings = 0;
+	var losings = 0;
 
-	while(arr.length < 4){
-    	var randomnumber = Math.ceil(Math.random()*12)
-    	
-    	if(arr.indexOf(randomnumber) > -1) continue;
-    	
-    	arr[arr.length] = randomnumber;
-	}
+	var crystalBlue = $("#crystalBlue");
+	var crystalRed = $("#crystalRed");
+	var crystalGreen = $("#crystalGreen");
+	var crystalYellow = $("#crystalYellow");
 
-	for (var i = 0; i < arr.length; i++) {
-		$(".imageCrystal").attr("data-crystalvalue", arr[i]);
-	
+	randomArray();
+
+	function randomArray () {
+		while(arr.length < 4){
+	    	var randomnumber = Math.ceil(Math.random()*12)
+	    	
+	    	if(arr.indexOf(randomnumber) > -1) continue;
+	    	
+	    	arr[arr.length] = randomnumber;
+		}
+
+		crystalBlue.attr("data-crystalvalue", arr[0]);
+		crystalRed.attr("data-crystalvalue", arr[1]);
+		crystalGreen.attr("data-crystalvalue", arr[2]);
+		crystalYellow.attr("data-crystalvalue", arr[3]);
+
+		
+	};
+
+	function resetGame() {
+		arr = [];
+		counter = 0;
+		targetNumber = Math.floor(Math.random()* (120) + 1);
+		$("#number-to-guess").text(targetNumber);
+		randomArray();	
 	}
 
 	$(".imageCrystal").on("click", function() {
 		var crystalValue = ($(this).attr("data-crystalvalue"));
     	crystalValue = parseInt(crystalValue);
     	counter += crystalValue;
-    	 alert("New score: " + counter);
+    	$("#totalScore").html(counter);
+    	// alert("New score: " + counter);
 
 	    if (counter === targetNumber) {
 	      alert("You win!");
-	      //reset Game
+	      winnings++;
+	      $("#winner").html(winnings);
+	      resetGame();
 	    }
 
 	    else if (counter >= targetNumber) {
 	      alert("You lose!!");
-
-	      //reset Game
+	      losings++;
+	      $("#loser").html(losings);
+		  resetGame();
 	    }
 	});
-
-	
-
-	console.log(arr);
 
 
 
